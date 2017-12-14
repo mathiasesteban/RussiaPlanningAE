dde94249e5215d342bdcecc0422c698fd194439b
Mathias Esteban <mesteban@fing.edu.uy>
2017-12-13 22:37:57 -0300

    graficarTodo script changed

--- RussiaPlanningGnuPlot/graficarTodo.sh
+++ RussiaPlanningGnuPlot/graficarTodo.sh
@@ -3,6 +3,6 @@
 gnuplot -e "
 set terminal png size 800,600;
 set output 'plot.png';
-FILES = system(\"ls -1 *.result\");
-LABEL = system(\"ls -1 *.result | sed -e 's/asd//' -e 's/.result//'\");
+FILES = system(\"ls -1 *.FUN\");
+LABEL = system(\"ls -1 *.FUN | sed -e 's/asd//' -e 's/.FUN//'\");
 plot for [i=1:words(FILES)] word(FILES,i) title word(LABEL,i) noenhanced;"
